1359b787ccc0505d3f97ee56c18e01458214a9ea
Danil8778 <dpnevsky@gmail.com>
2024-04-07 15:45:16 +0300

    Initial commit

---
+++ src/main/java/com/example/springvito/SpringvitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springvito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringvitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springvito/SpringvitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
